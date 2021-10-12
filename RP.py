7c63be1a243c0be1302286ba5fb323285692108f
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2021-10-12 20:42:55 +0530

    Made some changes to the function.

--- RP.py
+++ RP.py
@@ -1,15 +1,29 @@
 import psutil
 import os
 import time
+import sys
 
-while(True):
-    vMem = psutil.virtual_memory().percent
-    cpuUtil = psutil.cpu_percent()
-    cpuFreq = psutil.cpu_freq()
-    cpuTemp = psutil.sensors_temperture()
-    os.sep
-    diskUse = psutil.disk_usage(os.sep).percent
-    print("Memory used: "+ str(vMem), "| CPU used: " + str(cpuUtil), "| CPU core frequency: " + str(cpuFreq),"| Disk used: " + str(diskUse))
-    time.sleep(2)
-    if cpuUtil > 80.0:
-        print("WARNING! CPU USAGE HIGH")
+def start():
+    print("| Memory | CPU | Disk |")
+    while(True):
+        vMem = psutil.virtual_memory().percent
+        cpuUtil = psutil.cpu_percent()
+        cpuFreq = psutil.cpu_freq()
+        os.sep
+        diskUse = psutil.disk_usage(os.sep).percent
+        print("|  {}  | {} | {} |".format(str(vMem), str(cpuUtil), str(diskUse)))
+        if cpuUtil > 80.0:
+            print("WARNING! CPU USAGE HIGH")
+            delete_last_line
+        time.sleep(2)
+        delete_last_line()
+        
+
+def delete_last_line():
+    CURSOR_UP_ONE = '\x1b[1A'
+    ERASE_LINE = '\x1b[2K'
+    sys.stdout.write(CURSOR_UP_ONE)
+    sys.stdout.write(ERASE_LINE) 
+
+start()
+
