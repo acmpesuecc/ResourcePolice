931af0b8c8d8f0da34712884a047e11b68471fb1
Suhas KV <38615795+suhaskv1@users.noreply.github.com>
2021-10-13 21:48:03 +0530

    Testing a windows notification library

--- RP.py
+++ RP.py
@@ -2,8 +2,12 @@ import psutil
 import os
 import time
 import sys
+import win10toast as w10Notif
 
 def start():
+
+    /* This function starts the monitoring process */
+
     print("| Memory | CPU | Disk |")
     while(True):
         vMem = psutil.virtual_memory().percent
@@ -13,13 +17,19 @@ def start():
         diskUse = psutil.disk_usage(os.sep).percent
         print("|  {}  | {} | {} |".format(str(vMem), str(cpuUtil), str(diskUse)))
         if cpuUtil > 80.0:
-            print("WARNING! CPU USAGE HIGH")
-            delete_last_line
+            notification()
         time.sleep(2)
         delete_last_line()
         
+def notification():
+    n = w10Notif.ToastNotifier()
+
+    n.show_toast("ResourcePolice", "CPU Usage high!", duration = 10)
 
 def delete_last_line():
+
+    /* This function refreshes resource variables */
+
     CURSOR_UP_ONE = '\x1b[1A'
     ERASE_LINE = '\x1b[2K'
     sys.stdout.write(CURSOR_UP_ONE)
