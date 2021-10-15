ca0ef6a4e01e07800d505d65b162715933c529fe
AlphaDelta1803 <ipad4dasika@gmail.com>
2021-10-15 19:03:41 +0530

    Added monitors for Upload speed, Download Speed and CPU Frequency.

--- RP.py
+++ RP.py
@@ -4,21 +4,30 @@ import time
 import sys
 import win10toast as w10Notif
 
-TIME_DELAY = 1.5
+TIME_DELAY = 1.0
+prev_upload = int(psutil.net_io_counters(pernic=False)[0])
 
 def start():
+    # This function starts the monitoring process
 
-    # This function starts the monitoring process 
-
-    print("| Memory | CPU | Disk |")
-    while(True):
+    print("| Memory% | CPU%  | CPU(Hz) | Disk% | Upload(kB) | Download(kB) |")
+    prev_upload = int(psutil.net_io_counters(pernic=False)[0]/1024)
+    prev_download = int(psutil.net_io_counters(pernic=False)[1]/1024)
+    while True:
         vMem = psutil.virtual_memory().percent
         cpuUtil = psutil.cpu_percent()
-        cpuFreq = psutil.cpu_freq()
-        os.sep
-        diskUse = psutil.disk_usage(os.sep).percent
+        cpuFreq = psutil.cpu_freq()[1]/1000
+        sep = os.sep
+        new_upload = int(psutil.net_io_counters(pernic=False)[0]/1024)
+        upload = (new_upload - prev_upload)/TIME_DELAY
+        prev_upload = new_upload
+        new_download = int(psutil.net_io_counters(pernic=False)[1]/1024)
+        download = (new_download - prev_download)/TIME_DELAY
+        prev_download = new_download
+        diskUse = psutil.disk_usage(sep).percent
 
-        print("|  {}  | {} | {} |".format(str(vMem), str(cpuUtil), str(diskUse)))
+        print("| {0:>6}% | {1:^4}% | {2:>4}GHz | {3:>4}% | {4:>5}kBps | {5:>5}kBps  |".format(
+            str(vMem), str(cpuUtil), str(cpuFreq), str(diskUse), str(upload), str(download)))
 
         if cpuUtil > 80.0:
             CPUnotif()
@@ -31,7 +40,8 @@ def start():
 
         time.sleep(TIME_DELAY)
         delete_last_line()
-        
+
+
 def CPUnotif():
 
     # This function is to send in alert notifications in case of high CPU usage
@@ -61,7 +71,7 @@ def diskNotif():
 #     'rp.stop': stop,
 #     'rp.quit': quit,
 #     'rp.network': showNetworkResource
-# } # lookup table for user-defined commands 
+# } # lookup table for user-defined commands
 
 def stop():
 
@@ -88,7 +98,8 @@ def delete_last_line():
     CURSOR_UP_ONE = '\x1b[1A'
     ERASE_LINE = '\x1b[2K'
     sys.stdout.write(CURSOR_UP_ONE)
-    sys.stdout.write(ERASE_LINE) 
+    sys.stdout.write(ERASE_LINE)
 
 start()
+
 
