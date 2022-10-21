d4c8a7147eb304358ac61ed6fbec050b4972b5c5
suhaskv1 <suhaskv0111@gmail.com>
2022-10-21 19:06:56 -0400

    network module added

--- RP.py
+++ RP.py
@@ -4,14 +4,15 @@ import time
 import sys
 import plyer
 import platform
+import subprocess
 
 TIME_DELAY = 1
 
-
 def start():
+    '''
+    This function starts the monitoring process
+    '''
 
-    # This function starts the monitoring process
-    
     print("\n")
     print("="*50)
     print("System Resources:".center(50))
@@ -36,10 +37,12 @@ def start():
 
         # outDisp = "|  {}  | {} | {} |".format(
             # str(vMem), str(cpuUtil), str(diskUse))
-        
+
         # print(outDisp.center(40))
         print('{:<12.1f}{:>12.1f}{:>12.1f}'.format(vMem,cpuUtil,diskUse))
 
+        netstat = netStatDisplay()
+        print(netstat)
         if cpuUtil > 80.0:
             CPUnotif()
 
@@ -52,37 +55,44 @@ def start():
         time.sleep(TIME_DELAY)
         delete_last_line()
 
-
 def CPUnotif():
-
-    # This function is to send in alert notifications in case of high CPU usage for 10s
+    '''
+    This function is to send in alert notifications in case of high CPU usage for 10s
+    '''
 
     plyer.notification.notify("ResourcePolice", "CPU Usage high!", timeout=10)
 
 
 def memNotif():
-
-    # This function is to send in alert notifications in case of high RAM usage for 10s
+    '''
+    This function is to send in alert notifications in case of high RAM usage for 10s
+    '''
 
     plyer.notification.notify("ResourcePolice", "Memory Usage high!", timeout=10)
 
 
 def diskNotif():
-
-    # This function is to send in alert notifications in case of high disk usage for 10s
+    '''
+    This function is to send in alert notifications in case of high disk usage for 10s
+    '''
 
     plyer.notification.notify("ResourcePolice", "Disk Usage high!", timeout=10)
 
 def delete_last_line():
-
-    # This function refreshes resource variables
+    '''
+    This function refreshes resource variables
+    '''
 
     CURSOR_UP_ONE = '\x1b[1A'
     ERASE_LINE = '\x1b[2K'
-    sys.stdout.write(CURSOR_UP_ONE)
-    sys.stdout.write(ERASE_LINE)
+    for i in range(4):
+        sys.stdout.write(CURSOR_UP_ONE)
+        sys.stdout.write(ERASE_LINE)
 
 def fetchSystemInfo():
+    '''
+    Fetches system information
+    '''
     print("\n")
     print("="*50)
     print("System Information:".center(50))
@@ -96,9 +106,23 @@ def fetchSystemInfo():
     print(f"Processor: {uname.processor}")
 
 def introScreenLogo():
-    os.system("cls")
+    '''
+    Prints intro screen
+    '''
+    os.system("cls") # fix cls issue
     f = open('rplogo.txt', 'r')
     print(''.join([line for line in f]))
+
+def netStatDisplay():
+    reply = subprocess.run(['ping','-c','1','www.google.com'],
+                           stdout = subprocess.PIPE,
+                           stderr = subprocess.PIPE,
+                           encoding = 'utf-8')
+    if reply.returncode == 0:
+        return True, reply.stdout
+    else:
+        return False, reply.stderr
+    return reply
 
 introScreenLogo()
 fetchSystemInfo()
